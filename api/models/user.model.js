440543db228af369979106eb75773dec5bd9c9d6
PB <pranitabirajdar09@gmail.com>
2025-08-04 21:34:09 +0530

    completed real estate project

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new  mongoose.Schema({
+    username: {
+        type: String,
+        required:true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required:true,
+    },
+    avatar: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+        //another link:"https://www.w3schools.com/howto/img_avatar.png"
+    },
+},  
+ { timestamps: true});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
